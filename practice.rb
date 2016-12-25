14386d6e7814d0656a7d80997aaf3c5c0814ce59
Alex Afeltra <afeltra.alex@gmail.com>
2016-12-25 21:14:06 +0000

    booleans

--- practice.rb
+++ practice.rb
@@ -7,7 +7,7 @@ puts 20.0 / 10
 puts Math.sqrt(4)
 puts "hello world"
 puts "hello my name is " + "Alex" #string contatination
-=end
+
 
 #Variables and Interpolation
 name_1 = "Johnny"
@@ -16,4 +16,18 @@ puts "Hello my name is #{name_1}" #string interpolation
 puts "hello".length
 puts "hi there".capitalize
 puts "whats up".byteslice(1, 4)
+=end
 
+#Booleans
+puts 1 == 1
+puts 1 == 2
+puts 1 != 1
+puts 1 != 2
+puts 1 > 2
+puts 1 < 2
+puts 1 <= 1
+
+boolean1 = true
+boolean2 = false
+puts boolean1 && boolean2 #computes to true if both are true
+puts boolean1 || boolean2 #computers to true if one is true
