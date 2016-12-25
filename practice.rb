9586085420e5941954903738e1b8903d694b9794
Alex Afeltra <afeltra.alex@gmail.com>
2016-12-25 19:38:08 +0000

    variables and interpolation

--- practice.rb
+++ practice.rb
@@ -1,3 +1,4 @@
+=begin
 #Numbers and Strings
 puts 2 + 2
 puts 4 - 2
@@ -6,5 +7,13 @@ puts 20.0 / 10
 puts Math.sqrt(4)
 puts "hello world"
 puts "hello my name is " + "Alex" #string contatination
+=end
 
+#Variables and Interpolation
+name_1 = "Johnny"
+puts name_1
+puts "Hello my name is #{name_1}" #string interpolation
+puts "hello".length
+puts "hi there".capitalize
+puts "whats up".byteslice(1, 4)
 
