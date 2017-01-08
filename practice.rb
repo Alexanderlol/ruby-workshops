f5f887f6beabc51b6f429c15dc30d1846d1f7f57
Alex Afeltra <afeltra.alex@gmail.com>
2017-01-08 22:56:46 +0000

    used string interpolation to parse all 3 uris

--- practice.rb
+++ practice.rb
@@ -1,28 +1,25 @@
 require 'open-uri'
 require 'json'
-require 'pp'
 
-json_object = JSON.parse(open("https://shopicruit.myshopify.com/admin/orders.json?page=1&access_token=c32313df0d0ef512ca64d5b336a0d7c6").read)
-json_object2 = JSON.parse(open("https://shopicruit.myshopify.com/admin/orders.json?page=2&access_token=c32313df0d0ef512ca64d5b336a0d7c6").read)
-json_object3 = JSON.parse(open("https://shopicruit.myshopify.com/admin/orders.json?page=3&access_token=c32313df0d0ef512ca64d5b336a0d7c6").read)
+ORDER_PAGES = 3
+revenue = 0
 
-total1 = 0
-total2 = 0
-total3 = 0
+# loop thru 3 pages of orders
+for i in 1..ORDER_PAGES
+	total = 0
 
-json_object["orders"].each do |order|
-  total1 += order["total_price"].to_f
+	# fetch order
+	json_object = JSON.parse(open("https://shopicruit.myshopify.com/admin/orders.json?page=#{i}&access_token=c32313df0d0ef512ca64d5b336a0d7c6").read)
+
+	# iterate thru each order cost
+	json_object["orders"].each do |order|
+		total += order["total_price"].to_f
+	end
+
+	# round to 2 decimal points
+	puts total.round(2)
+
+	revenue += total
 end
 
-json_object2["orders"].each do |order|
-  total2 += order["total_price"].to_f
-end
-
-json_object3["orders"].each do |order|
-  total3 += order["total_price"].to_f
-end
-
-pp total1.round(2)
-pp total2.round(2)
-pp total3.round(2)
-pp total1.round(2) + total2.round(2) + total3.round(2)
+puts revenue.round(2)
